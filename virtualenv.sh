a9f0ff5a10de44f2dfdc6b130debb3823fd98d85
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-09-04 03:14:54 -0400

    fixed bashrc

--- virtualenv.sh
+++ virtualenv.sh
@@ -4,5 +4,9 @@ sudo pip3 install virtualenvwrapper
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
 
 export WORKON_HOME=~/.Envs
+echo 'WORKON_HOME=~/.Envs' >> ~/.bashrc
+
+echo 'VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3' >> ~/.bashrc
 mkdir -p $WORKON_HOME
 source ~/.local/bin/virtualenvwrapper.sh
+echo 'source ~/.local/bin/virtualenvwrapper.sh' >> ~/.bashrc
