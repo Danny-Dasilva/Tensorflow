18076956b2207c60e8c2b68da6d2e6c10d9eb001
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-09-04 02:48:55 -0400

    added virtualenv

--- virtualenv.sh
+++ virtualenv.sh
@@ -1,4 +1,8 @@
-pip3 install virtualenvwrapper
-export WORKON_HOME=~/Envs
+sudo pip3 install virtualenv
+
+sudo pip3 install virtualenvwrapper
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+
+export WORKON_HOME=~/.Envs
 mkdir -p $WORKON_HOME
-source /usr/local/bin/virtualenvwrapper.sh
+source ~/.local/bin/virtualenvwrapper.sh
