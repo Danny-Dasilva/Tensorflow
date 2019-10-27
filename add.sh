d522d302a8a6d8d7997741af948918a9b5ec5e76
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-10-27 04:38:05 -0400

    test

--- add.sh
+++ add.sh
@@ -1,3 +1,12 @@
+dir=$PWD
+
+chmod a+x "$dir"/Venv/check_venv.sh
+chmod a+x "$dir"/Venv/create_virtualenv.sh
+
+read -p 'Write the name your virtual environment: ' obs_input
+echo
+echo $obs_input
+
 source Venv/check_venv.sh
 
 workon ${obs_venv}
