4570380ac907b8e2ab568f5bdf1d9bd08e6f91c1
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-11-29 16:33:48 -0500

    added check for duplcates

--- convert.py
+++ convert.py
@@ -1,8 +1,13 @@
 from PIL import Image
 import os
 path = os.getcwd()
-images = []
+xml_list  []
+for xml in os.listdir(path):
+    if xml.endswith('xml'):
+        xml_list.append(os.path.splitext(xml)[0])
+
 for filename in os.listdir(path):
     if filename.endswith('.jpg'):
-        im = Image.open(filename)
-        im.save(os.path.splitext(filename)[0]+ '.png')
+        if os.path.splitext(filename)[0] in xml_list
+            im = Image.open(filename)
+            im.save(os.path.splitext(filename)[0]+ '.png')
